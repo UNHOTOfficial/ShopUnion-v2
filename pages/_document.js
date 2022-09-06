e700c5ae4af1602ccbb586549395e470a37b3ee6
Unhot <serverings@protonmail.com>
2022-09-06 14:51:31 +0430

    Add _document, Initialized Header And Footer.

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        {/* <Script src="https://tailwindui.com/js/alpine.js?id=aec657470ad97cbba5c2659fd719c2de" strategy="beforeInteractive"></Script> */}
+      </body>
+    </Html>
+  );
+}
