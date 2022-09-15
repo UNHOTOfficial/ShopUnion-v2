d2b408e943b2cb877ac2f944865f6b60d11b2e1b
Unhot <serverings@protonmail.com>
2022-09-15 17:30:04 +0430

    Fix Light Mode Background Color.

--- components/headerDeals.jsx
+++ components/headerDeals.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function HeaderDeals() {
   return (
-    <div className="text-gray-900 bg-gray-800 py-1 text-center capitalize text-sm font-thin dark:text-white">
+    <div className="text-gray-900 bg-slate-200 py-1 text-center capitalize text-xs md:text-sm font-thin dark:text-white dark:bg-gray-800">
       get up to 50% off on offer products.
       <Link href="/offers">
         <a className="underline ml-1 hover:no-underline">Offers</a>
