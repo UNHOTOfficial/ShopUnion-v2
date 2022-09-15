feffdddabfb40af07087601a80dfff1d819cd611
Unhot <serverings@protonmail.com>
2022-09-15 17:33:01 +0430

    Add Viewport Meta Tag.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "../styles/globals.scss";
 import Layout from "../components/layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
