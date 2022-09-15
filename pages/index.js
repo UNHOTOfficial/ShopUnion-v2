1ce206f80e2a17675d87ccab54fbb9b6aa79078a
Unhot <serverings@protonmail.com>
2022-09-15 14:00:31 +0430

    Add Offers Scroll Menu And Categories To Home.

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,38 @@ import Head from "next/head";
 import React from "react";
 import Carousel from "../components/carousel";
 import GetProducts from "../services/GetProducts";
-export default function Home({sliderImages}) {
+import ScrollMenu from "../components/scrollMenu";
+import HomeCategories from "../components/homeCategories";
+export default function Home({ sliderImages, offerProducts, categories }) {
   return (
     <>
       <Head>
         <title>ShopUnion</title>
       </Head>
       <div>
-        <Carousel sliderImages={sliderImages}/>
+        <Carousel sliderImages={sliderImages} />
+        <HomeCategories categories={categories} />
+        <ScrollMenu offerProducts={offerProducts} />
       </div>
     </>
   );
 }
 
 export async function getServerSideProps() {
-  const sliderImages = await GetProducts("https://apigenerator.dronahq.com/api/25-FaSpo/sliderz");
-
+  const sliderImages = await GetProducts(
+    "https://apigenerator.dronahq.com/api/25-FaSpo/sliderz"
+  );
+  const offers = await GetProducts(
+    "https://apigenerator.dronahq.com/api/U8Y0Ntkw/offers"
+  );
+  const categories = await GetProducts(
+    "https://apigenerator.dronahq.com/api/ImisGQ9N/categories"
+  );
   return {
     props: {
       sliderImages: sliderImages,
+      offerProducts: offers,
+      categories: categories,
     },
   };
 }
