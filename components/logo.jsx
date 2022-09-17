05ca72d2947644981d99723346a4828f50754996
Unhot <serverings@protonmail.com>
2022-09-17 13:17:43 +0430

    Convert Logo To Individual Component.

---
+++ components/logo.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <a className="flex">
+        <Image
+          src="/assest/logo/image.svg"
+          width="50"
+          height="50"
+          alt="Logo"
+          layout="fixed"
+          placeholder="blur"
+          blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+        />
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          ShopUnion
+        </span>
+      </a>
+    </Link>
+  );
+}
