30d8dee3dda6c0589ce5aa083f6d78ea85ef9e88
Unhot <serverings@protonmail.com>
2022-09-22 14:14:37 +0330

    Fix Add To Cart Button.

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -123,7 +123,29 @@ export default function Product({ product }) {
               </button>
             </div>
           ) : (
-            <span>${Math.floor((product.price * 100) / 100)}</span>
+            <div className="flex justify-between items-center">
+              <span>${Math.floor((product.price * 100) / 100)}</span>
+              <button
+                type="button"
+                className="flex items-center justify-center text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2 md:px-12 text-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 mr-1 lg:hidden xl:block"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+                Add To Cart
+              </button>
+            </div>
           )}
         </div>
 
