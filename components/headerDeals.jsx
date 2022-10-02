399e2dc1ddb9392a808b761f232b203987435e65
Unhot <serverings@protonmail.com>
2022-10-02 20:27:01 +0330

    Add Border.

--- components/headerDeals.jsx
+++ components/headerDeals.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function HeaderDeals() {
   return (
-    <div className="text-gray-900 bg-slate-200 py-1 text-center capitalize text-xs md:text-sm font-thin dark:text-white dark:bg-gray-800">
+    <div className="border-y border-gray-900 dark:border-gray-500 text-gray-900 bg-slate-200 py-1 text-center capitalize text-xs md:text-sm font-thin dark:text-white dark:bg-gray-800">
       get up to 50% off on offer products.
       <Link href="/offers">
         <a className="underline ml-1 hover:no-underline">Offers</a>
