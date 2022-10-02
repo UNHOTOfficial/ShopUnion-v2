a3fef7c9cd96f0631a8ddbdddb5ddadeaaf14ddc
Unhot <serverings@protonmail.com>
2022-10-02 21:17:11 +0330

    Add Sign Up Page.

---
+++ pages/signup.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+
+export default function signup() {
+  return (
+    <>
+      <Head>
+        <title>ShopUnion</title>
+        <meta name="description" content="Create Account In ShopUnion." />
+      </Head>
+<div>Sign Up</div>
+    </>
+  );
+}
