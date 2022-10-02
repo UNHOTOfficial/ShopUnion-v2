8bd21a83e79e5966bbb0da3ef91574dff4713ff2
Unhot <serverings@protonmail.com>
2022-10-02 21:15:59 +0330

    Add Sign In Link.

--- components/header.jsx
+++ components/header.jsx
@@ -210,19 +210,23 @@ export default function Header() {
                 </button>
               </li>
               <li>
-                <button
-                  type="button"
-                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
-                  Get Started
-                </button>
+                <Link href="/signup">
+                  <button
+                    type="button"
+                    className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Get Started
+                  </button>
+                </Link>
               </li>
               <li>
-                <button className="inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                  <span className="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    Sign In
-                  </span>
-                </button>
+                <Link href="/signin">
+                  <button className="inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                    <span className="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Sign In
+                    </span>
+                  </button>
+                </Link>
               </li>
             </ul>
           </div>
