2faf6367272a5d64d4a9cb3054058b2ae68ab734
Unhot <serverings@protonmail.com>
2022-10-04 14:58:55 +0330

    Add Categories Menu Item.

--- components/headerSecondary.jsx
+++ components/headerSecondary.jsx
@@ -3,11 +3,27 @@ import React from "react";
 
 export default function HeaderSecondary() {
   return (
-    <nav className="bg-slate-300 px-4 text-sm sm:px-4 dark:bg-gray-800 border-y dark:border-gray-500 border-gray-900">
+    <nav className="bg-slate-300 px-4 text-sm sm:px-4 dark:bg-gray-800 shadow-xl">
       <div className="container flex items-center text-gray-900 dark:text-white">
-        <ul className="flex w-2/3 md:w-1/3 xl:w-1/4 justify-between">
+        <ul className="flex w-3/4 md:w-1/3 xl:w-1/4 justify-between">
           <li className="hover:underline">
-            <Link href="/">Home</Link>
+            <div className="flex cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 mr-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+                />
+              </svg>
+              Categories
+            </div>
           </li>
           <li className="hover:underline">
             <Link href="/offers">Offers</Link>
