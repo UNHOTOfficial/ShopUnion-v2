f680b72678ff75997194d636233bb272a1303d73
Unhot <serverings@protonmail.com>
2022-11-09 00:39:35 +0330

    Responsive.

--- components/homeCategories.jsx
+++ components/homeCategories.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function homeCategories({ categories }) {
   return (
-    <div className="text-gray-900 flex py-3 dark:text-white dark:bg-gray-900">
+    <div className="text-gray-900 flex justify-center md:justify-around py-6 dark:text-white dark:bg-gray-900">
       {categories.map((category) => (
         <Link href={`/products/${category.title}`} key={category.title}>
           <a>
@@ -14,14 +14,14 @@ export default function homeCategories({ categories }) {
                 src={category.url}
                 alt={category.title}
                 quality="100"
-                width={50}
-                height={50}
+                width={80}
+                height={80}
                 layout="fixed"
                 objectFit="cover"
                 // placeholder="blur"
                 // blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
               />
-              <span className="capitalize mt-1">{category.title}</span>
+              <span className="capitalize mt-1 text-center">{category.title}</span>
             </div>
           </a>
         </Link>
